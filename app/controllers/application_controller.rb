3ac5103e1886abec9037ffa04ac22fd398199696
at-dungnguyen3 <dung.nguyen3@asiantech.vn>
2018-08-01 14:34:50 +0700

    Implement advanced login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
   def not_found
     raise ActionController::RoutingError, 'Not Found'
   rescue StandardError
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,4 +9,51 @@ RSpec.describe SessionsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'POST #create' do
+    context 'login success' do
+      it 'redirect to user' do
+        user = User.create(name: 'Dũng', email: 'test@gmail.com', password: '123456', password_confirmation: '123456')
+        post :create, params: { session: { email: 'test@gmail.com', password: '123456' } }
+        expect(response).to redirect_to(user)
+      end
+    end
+
+    context 'login wrong email' do
+      it 'render login' do
+        user = User.create(name: 'Dũng', email: 'test@gmail.com', password: '123456', password_confirmation: '123456')
+        post :create, params: { session: { email: 'test2@gmail.com', password: '123456' } }
+        expect(response).to render_template(:new)
+        expect(flash.now[:danger]).to eq('Invalid email/password combination')
+      end
+    end
+
+    context 'login wrong password' do
+      it 'render login' do
+        user = User.create(name: 'Dũng', email: 'test@gmail.com', password: '123456', password_confirmation: '123456')
+        post :create, params: { session: { email: 'test@gmail.com', password: '1234567' } }
+        expect(response).to render_template(:new)
+        expect(flash.now[:danger]).to eq('Invalid email/password combination')
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    context 'logout success' do
+      it 'redirect to root' do
+        user = User.create(name: 'Dũng', email: 'test@gmail.com', password: '123456', password_confirmation: '123456')
+        session[:user_id] = user.id
+        delete :destroy
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    # context 'logout error' do
+    #   it 'redirect to login' do
+    #     user = User.create(name: 'Dũng', email: 'test@gmail.com', password: '123456', password_confirmation: '123456')
+    #     delete :destroy, params: { sesssion: nil }
+    #     expect(response).to redirect_to(login_path)
+    #   end
+    # end
+  end
 end
